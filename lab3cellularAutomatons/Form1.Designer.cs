e6eee3dc99334b2471b46524395c13673885f83d
rinaAks <128968330+rinaAks@users.noreply.github.com>
2024-02-28 17:09:22 +0700

    Add project files.

---
+++ lab3cellularAutomatons/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace lab3cellularAutomatons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btRandom = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.matrixGrid1 = new lab3cellularAutomatons.MatrixGrid();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.flowLayoutPanel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.edRule);
+            this.panel2.Controls.Add(this.btStart);
+            this.panel2.Controls.Add(this.btStop);
+            this.panel2.Controls.Add(this.btRandom);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(222, 450);
+            this.panel2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rule №";
+            // 
+            // edRule
+            // 
+            this.edRule.Location = new System.Drawing.Point(80, 76);
+            this.edRule.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(80, 22);
+            this.edRule.TabIndex = 1;
+            this.edRule.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(80, 181);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Старт";
+            this.btStart.UseVisualStyleBackColor = true;
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(80, 233);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(75, 23);
+            this.btStop.TabIndex = 3;
+            this.btStop.Text = "Стоп";
+            this.btStop.UseVisualStyleBackColor = true;
+            // 
+            // btRandom
+            // 
+            this.btRandom.Location = new System.Drawing.Point(51, 329);
+            this.btRandom.Name = "btRandom";
+            this.btRandom.Size = new System.Drawing.Size(123, 54);
+            this.btRandom.TabIndex = 7;
+            this.btRandom.Text = "Сгенерировать рандомно";
+            this.btRandom.UseVisualStyleBackColor = true;
+            this.btRandom.Click += new System.EventHandler(this.btRandom_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.matrixGrid1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(225, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(575, 447);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // matrixGrid1
+            // 
+            this.matrixGrid1.GridSize = new System.Drawing.Size(20, 20);
+            this.matrixGrid1.Location = new System.Drawing.Point(3, 3);
+            this.matrixGrid1.Name = "matrixGrid1";
+            this.matrixGrid1.Size = new System.Drawing.Size(572, 444);
+            this.matrixGrid1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MatrixGrid matrixGrid1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btRandom;
+    }
+}
+
---
+++ lab3cellularAutomatons/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3cellularAutomatons
+{
+    public partial class Form1 : Form
+    {
+        private byte[,] cells;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cells = new byte[,] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            var grid = new MatrixGrid() 
+            { 
+                Parent = matrixGrid1,
+                Dock = DockStyle.Top,
+                GridSize = new Size(6, 3), 
+                BorderStyle = BorderStyle.FixedSingle, 
+                Location = new Point(100, 100) 
+            };
+            /*var grid = new MatrixGrid() {
+                Parent = flowLayoutPanel2,
+                Dock = DockStyle.Fill,
+                GridSize = new Size(30, 30),
+                BorderStyle = BorderStyle.FixedSingle
+                ,
+                Location = new Point(250, 250)
+            };*/
+
+            //matrixGrid1 = grid;
+
+            //создаем матрицу
+            /*string[,] matrix = new string[10, 20];
+            matrix[1, 4] = "AA";
+            matrix[2, 3] = "BB";
+            
+            //создаем грид
+            var grid = new MatrixGrid() { Parent = matrixGrid1, Dock = DockStyle.Fill };
+            */
+            grid.CellNeeded += grid_CellNeeded;
+            grid.CellClick += grid_CellClick;
+            //задаем размер грида
+            //grid.GridSize = new Size(matrix.GetLength(0), matrix.GetLength(1));
+
+            //grid.Visible = true;
+
+            //присваиваем событие в котором будем отдавать текст ячейки и ее цвет
+            /*grid.CellNeeded += (o, e) =>
+            {
+                e.Value = matrix[e.Cell.X, e.Cell.Y];
+                e.BackColor = Color.FromArgb(100, (e.Cell.X * 10) % 256, (e.Cell.Y * 10) % 256, (e.Cell.X * 10) % 256);
+                //e.BackColor = Color.Red;
+                //e.BackColor = cells[e.Cell.X, e.Cell.Y] > 0 ? Color.Red : Color.White;
+            };*/
+
+            //обрабатываем событие клика по ячейке
+            /*grid.CellClick += (o, e) =>
+            {
+                MessageBox.Show(e.Cell.ToString());
+                //cells[e.Cell.X, e.Cell.Y] = (byte)(1 - cells[e.Cell.X, e.Cell.Y]);
+            };*/
+        }
+        
+        void grid_CellClick(object sender, MatrixGrid.CellClickEventArgs e)
+        {
+            cells[e.Cell.X, e.Cell.Y] = (byte)(1 - cells[e.Cell.X, e.Cell.Y]);
+        }
+
+        void grid_CellNeeded(object sender, MatrixGrid.CellNeededEventArgs e)
+        {
+            e.BackColor = cells[e.Cell.X, e.Cell.Y] > 0 ? Color.Green : Color.White;
+        }
+
+        Random rnd = new Random();
+
+        private void btRandom_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cells.GetLength(0); i++)
+                cells[i, 0] = (byte)rnd.Next(0, 2);
+
+            Invalidate(true);
+        }
+    }
+}
